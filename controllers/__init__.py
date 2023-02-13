df685f6b0bf1bc50d376c8a7fc36de8d507a55c8
AndresBo <anndresfborrero@gmail.com>
2023-02-13 21:25:45 +1000

    added movies Blueprint controller

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,6 +1,8 @@
 from controllers.actors_controller import actors
+from controllers.movies_controller import movies
 
 
 registerable_controllers = [
     actors,
+    movies
 ]
---
+++ controllers/movies_controller.py
@@ -0,0 +1,55 @@
+from flask import Blueprint, jsonify, request
+from main import db
+from models.movies import Movie
+
+
+movies = Blueprint('movies', __name__, url_prefix="/movies")
+
+
+@movies.route("/", methods = ["GET"])
+def get_actors():
+    movies_list = Movie.query.all()
+    # esult = movies_schema.dump(movies_list)
+    # return jsonify(result)
+    return "list of movies retrieved"
+
+
+@movies.route("/", methods=["POST"])
+#@jwt_required()
+def movie_create():
+    # movie_fields = movie_schema.load(request.json)
+
+    # new_movie = Movie()
+    # new_movie.title = movie_fields["title"]
+    # new_movie.genre = movie_fields["genre"]
+    # new_movie.length = movie_fields["length"]
+    # new_movie.year = movie_fields["year"]
+    
+    # db.session.add(new_movie)
+    # db.session.commit()
+
+    # return jsonify(movie_schema.dump(new_movie))
+    return "card created"
+
+
+@movies.route("/<int:id>/", methods=["DELETE"])
+#@jwt_required()
+
+def movie_delete(id):
+    # user_id = get_jwt_identity()
+    # user = User.query.get(user_id)
+
+    # if not user:
+    #     return abort(401, description="Invalid user")
+
+    # movie = Movie.query.filter_by(id=id).first()
+
+    # if not Movie:
+    #     return abort(400, description= "Movie does not exists")
+
+    # db.session.delete(movie)
+    # db.session.commit()
+
+    # return jsonify(movie_schema.dump(movie))
+    return "movie deleted"
+    
