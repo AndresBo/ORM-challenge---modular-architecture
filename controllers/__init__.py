8aaf4d4d172a7089e821558bdf72d9aa8fca48e8
AndresBo <anndresfborrero@gmail.com>
2023-02-14 14:10:29 +1000

    added authentication: signin and signup

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,8 +1,10 @@
 from controllers.actors_controller import actors
 from controllers.movies_controller import movies
+from controllers.auth_controller import auth
 
 
 registerable_controllers = [
     actors,
-    movies
+    movies,
+    auth
 ]
