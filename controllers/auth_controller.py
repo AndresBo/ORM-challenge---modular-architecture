379d655f999f3bc498bcdfbca7d131d6688ae9f5
AndresBo <anndresfborrero@gmail.com>
2023-02-14 13:41:18 +1000

    modified user_schema.py

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,1 +1,4 @@
 from flask import Blueprint, jsonify, request, abort
+from main import db
+from models.user import User
+from schemas.user_schema import 
--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -1,10 +1,12 @@
-# from main import ma
+from main import ma
+from models.user import User
+from marshmallow.validate import Length
 
 
-# class UserSchema(ma.SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = User
-#     password = ma.String(validate=Length(min=8))
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+    password = ma.String(validate=Length(min=8))
 
-# user_schema = UserSchema()
-# users_schema = UserSchema(many=True)
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
